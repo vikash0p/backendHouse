f78e8481550bcfe419a308e031976e27fb05d670
Vikash <119168528+vikash0p@users.noreply.github.com>
2025-01-02 07:54:57 +0530

    add cart schema model to manage user shopping cart items

---
+++ mvc/models/cartSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true, min: 1 },
+    price: { type: Number, required: true }, // Price of the item at the time of adding to cart
+});
+
+const cartSchema = new mongoose.Schema(
+    {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        items: [cartItemSchema],
+        totalAmount: { type: Number, required: true, default: 0 }, // Calculated value
+    },
+    { timestamps: true }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+export default Cart;
